4ba8e40afb67f3d34cc80506e64ff3e8f37e0c67
Daniel <daniel.meneses@osaango.com>
2023-05-23 14:41:18 +0300

    rps exercise

---
+++ rps/remote-power-supply.robot
@@ -0,0 +1,30 @@
+*** Settings ***
+Library      commands.py
+
+*** Keywords ***
+RPS send commands
+    [Arguments]    ${command}    ${port}    ${state}
+    ${output}=    Send cmds    ${command}  ${port}  ${state}
+    [return]    ${output}
+
+RPS get Power
+    [Arguments]    ${command}
+    ${output}=    Send cmds    ${command}
+    [return]    ${output}
+
+*** Test Cases ***
+Turn on Power supply Helsinki remotely
+     ${out}=    RPS send commands     SetPower  Helsinki  1
+     Should be equal    ${out}  ${True}
+
+Verify power supply Helsinki is on
+    ${out}=     RPS get power    GetPower
+    should contain    ${out}  Helsinki=1
+
+Turn off Power supply Espoo remotely
+     ${out}=    RPS send commands     SetPower  Espoo  0
+     Should be equal    ${out}  ${True}
+
+Verify power supply Espoo is off
+    ${out}=     RPS get power    GetPower
+    should contain    ${out}  Espoo=0
---
+++ rps/rps-server.py
@@ -0,0 +1,53 @@
+from flask import Flask, request
+from flask_httpauth import HTTPBasicAuth
+
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+        'admin': '12345678'
+}
+app.url_map.strict_slashes = False
+
+PINS = ['Helsinki', 'Espoo', 'Vantaa', 'Sipoo']
+
+PINS_STATUS = {'Helsinki':'0', 'Espoo': '0', 'Vantaa':'0', 'Sipoo':'0'}
+
+@auth.get_password
+def get_pw(username):
+        if username in users:
+                return users.get(username)
+        return None
+
+@app.route('/')
+@auth.login_required
+def index():
+        return "Hello, %s!" % auth.username()
+
+def get_html_string():
+        html_str = '<h1> *Helsinki={}  *Espoo={}  *Vantaa={}  *Sipoo={} </h1>'.format(PINS_STATUS['Helsinki'],
+                                                PINS_STATUS['Espoo'],
+                                            PINS_STATUS['Vantaa'],
+                                            PINS_STATUS['Sipoo'])
+        return html_str
+
+def parse_cmd_args(args):
+        global current_status
+        if str(args['CMD']) == 'SetPower':
+                for key in args:
+                        if key in PINS:
+                                PINS_STATUS[key] = str(args[key])
+                            
+                return get_html_string()
+    
+        if str(args['CMD']) == 'GetPower':
+                return get_html_string()
+   
+
+@app.route('/SetCmd', methods=['GET','POST'])
+def rps():
+    if request.method=="GET":
+            args=request.args.to_dict()
+            ret = parse_cmd_args(args)
+            return ret
